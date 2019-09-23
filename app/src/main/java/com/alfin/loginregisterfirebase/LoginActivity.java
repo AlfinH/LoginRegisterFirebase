79752ce44e1849534361e0c5a143cce976dbc5f0
alfin-jq <alfinherianto8@gmail.com>
2019-09-23 21:54:05 +0700

    App Done!
    Login, Register, Logout functions has been added.

--- app/src/main/java/com/alfin/loginregisterfirebase/LoginActivity.java
+++ app/src/main/java/com/alfin/loginregisterfirebase/LoginActivity.java
@@ -1,14 +1,103 @@
 package com.alfin.loginregisterfirebase;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText edLoginEmail,edLoginPassword;
+    private Button btnLogin;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        edLoginEmail = findViewById(R.id.ed_login_email);
+        edLoginPassword = findViewById(R.id.ed_login_password);
+        btnLogin = findViewById(R.id.btn_login);
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+                if(mFirebaseUser != null){
+                    Toast.makeText(LoginActivity.this, "You are logged in", Toast.LENGTH_SHORT).show();
+                    goToMainAct();
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "Please Login", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = edLoginEmail.getText().toString().trim();
+                String password = edLoginPassword.getText().toString();
+                if(email.isEmpty()){
+                    edLoginEmail.setError("Please enter your email");
+                    edLoginEmail.requestFocus();
+                }
+                else if(password.isEmpty()) {
+                    edLoginPassword.setError("Please enter your password");
+                    edLoginPassword.requestFocus();
+                }
+                else if(!email.isEmpty() && !password.isEmpty()){
+                    mFirebaseAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(!task.isSuccessful()){
+                                        Toast.makeText(LoginActivity.this, "Login Error, Please Login Again", Toast.LENGTH_SHORT).show();
+                                    }
+                                    else{
+                                        goToMainAct();
+                                    }
+                                }
+                            });
+                }
+                else {
+                    Toast.makeText(LoginActivity.this, "Error Occured!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    public void goRegisAct(View v){
+        Intent goRegis = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(goRegis);
+    }
+
+    private void goToMainAct() {
+        Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(mainIntent);
     }
 }
--- app/src/main/java/com/alfin/loginregisterfirebase/RegisterActivity.java
+++ app/src/main/java/com/alfin/loginregisterfirebase/RegisterActivity.java
@@ -1,14 +1,74 @@
 package com.alfin.loginregisterfirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText edRegisEmail,edRegisPassword;
+    private Button btnRegis;
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        edRegisEmail = findViewById(R.id.ed_regis_email);
+        edRegisPassword = findViewById(R.id.ed_regis_password);
+        btnRegis = findViewById(R.id.btn_regis);
+
+        btnRegis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = edRegisEmail.getText().toString().trim();
+                String password = edRegisPassword.getText().toString();
+                if(email.isEmpty()){
+                    edRegisEmail.setError("Please enter your new email");
+                    edRegisEmail.requestFocus();
+                }
+                else if(password.isEmpty()){
+                    edRegisPassword.setError("Please enter your new password");
+                    edRegisPassword.requestFocus();
+                }
+                else if(!email.isEmpty() && !password.isEmpty()){
+                    mFirebaseAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(RegisterActivity.this, "SignUp Unsuccessful, Please Try Again", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                            }
+                        }
+                    });
+                }
+                else {
+                    Toast.makeText(RegisterActivity.this, "Error Occured!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void goLoginAct(View v){
+        Intent goLogin = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(goLogin);
     }
 }
